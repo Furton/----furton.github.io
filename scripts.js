e65ef9247a0082d0184bbbf883659cc8b351bfae
Павел Петров <55802681+Furton@users.noreply.github.com>
2023-06-26 23:47:13 +0300

    fix

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ let gameScene = new Phaser.Scene('Game');
 // our game's configuration
 let config = {
     type: Phaser.AUTO,  //Phaser will decide how to render our game (WebGL or Canvas)
-    width: 640, // game width
-    height: 360, // game height
+    width: window.innerWidth, // game width
+    height: window.innerHeight, // game height
     scene: gameScene // our newly created scene
 };
 // create the game, and pass it the configuration
