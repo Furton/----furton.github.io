ca7a890dbd1dbdbf1479503df77cbbd9e1da3d82
Павел Петров <55802681+Furton@users.noreply.github.com>
2023-06-26 23:52:06 +0300

    fix

--- scripts.js
+++ scripts.js
@@ -2,9 +2,46 @@
 let gameScene = new Phaser.Scene('Game');
 // our game's configuration
 let config = {
-    type: Phaser.AUTO,  //Phaser will decide how to render our game (WebGL or Canvas)
-    width: window.innerWidth, // game width
-    height: window.innerHeight, // game height
+    // ...
+    parent: divId,
+
+    // Game size
+    width: 1024,
+    height: 768,
+
+    scale: {
+        // Or set parent divId here
+        parent: divId,
+
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+
+        // Or put game size here
+        // width: 1024,
+        // height: 768,
+
+        // Minimum size
+        min: {
+            width: 800,
+            height: 600
+        },
+        // Or set minimum size like these
+        // minWidth: 800,
+        // minHeight: 600,
+
+        // Maximum size
+        max: {
+            width: 1600,
+            height: 1200
+        },
+        // Or set maximum size like these
+        // maxWidth: 1600,
+        // maxHeight: 1200,
+
+        zoom: 1,  // Size of game canvas = game size * zoom
+    },
+    autoRound: false
+    // ...
     scene: gameScene // our newly created scene
 };
 // create the game, and pass it the configuration
